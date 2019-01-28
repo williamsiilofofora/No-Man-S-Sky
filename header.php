047a7d468ac1e5c4796ac204beffb9fdf916a618
Williams KUBIAK <wkubiak@561557n.ac-rennes.fr>
2019-01-28 13:21:43 +0100

    initialisation du projet

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="space.css" type="text/css">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <title>No Man's Sky</title>
+</head>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+
+session_start();
+
+
+include ('header.php');
+?>
+
+<!----------contenu---------->
+<body>
+    <div id="bandeauHaut">
+    <img src="image/logo-nms.png" id="logo"/>
+    <div id="nav">
+    <ul>
+   <li id="nav-home"><a href="#">Accueil</a></li>
+   <li id="nav-Presentation"><a href="#">Présentation</a></li>
+   <li id="nav-Communauté"><a href="#">Communauté</a></li>
+   <li id="nav-media"><a href="#">Media</a></li>
+   <li id="nav-calactic"><a href="https://galacticatlas.nomanssky.com/">Galatic Atlas</a></li>
+   <li id="nav-contact"><a href="#">Contact</a></li>
+    </ul>
+    </div>
+    </div>
+    <div>
+       <h1 class="zoomIn">
+            REJOINGNEZ LA COMMUNAUTEE
+       </h1>
+       <p><img src="image/animated-logo-red.gif" class="logoAnime" alt="logoNMS"/></p>
+      <!--animation-->
+        <p id="animationBG">
+			<span id="fog">A long</span>
+			<span id="fog-2">long time</span>
+			<span id="glow">ago,</span>
+			<span id="light">in a</span>
+			<span id="stars">galaxy</span>
+			<span id="stars-2">far</span>
+			<span id="small-stars">far</span>
+			<span id="small-stars-2">away…</span>
+		</p>
+    </div>  
+    <?php
+    //include('slider.php');
+    ?>
+    
+    
+    
+    
+    
+    
+
+
+
+
+<!-------menu-nav--------->
+</body>
+</html>
